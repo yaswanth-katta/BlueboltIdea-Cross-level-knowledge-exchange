f09bdfd885d0c88bb9b668615c95193406df0d92
yaswanth-katta <kattayaswanthkumar@gmail.com>
2024-06-12 18:59:21 +0530

    Modified groupDiscussion screen and added PNGs

--- src/app/group-discussion/group-discussion.component.ts
+++ src/app/group-discussion/group-discussion.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-group-discussion',
   templateUrl: './group-discussion.component.html',
   styleUrl: './group-discussion.component.css'
 })
-export class GroupDiscussionComponent {
+export class GroupDiscussionComponent implements OnInit {
+  message = "";
+  ngOnInit(): void {
+    this.message = `
+    Its went never ending loop!!
+    But i dont know where went wrog, here is the code.
+    while(i<=n){
+      if(i%2==0){
+        print even number
+      }else{
+        print odd number
+      }
+    }
+    `
+  }
+
+
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
 
 }
